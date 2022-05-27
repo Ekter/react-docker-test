37d9865bb473ee61897b25c0ab4f6b1f63928b66
Ekter <ektermoi1@gmail.com>
2022-05-27 12:32:08 +0200

    1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactukjhjkhkh
+          Learn Reactukjhjkhkhjghvbn
         </a>
       </header>
     </div>
